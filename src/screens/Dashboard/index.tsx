bfe383dc19ae76793c6d280dbdd57a1e4cf9acb4
Jonathas Ribeiro <jonathasribeiroreal@gmail.com>
2021-05-30 04:18:34 -0300

    feat: stop Chapter II-2:9

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
 
 
 import {
@@ -33,6 +34,8 @@ export function Dashboard() {
           <Icon name="power" />
         </UserWrapper>
       </Header>
+
+      <HighlightCard />
     </Container>
   );
 }
